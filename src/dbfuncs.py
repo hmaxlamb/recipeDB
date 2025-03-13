72f76dbfe3719db8e8ed325a5cd3ab4eb4449ef8
mlamb <h.max.lamb@gmail.com>
2025-03-12 21:49:28 -0400

    9. finished up DB structer

--- src/dbfuncs.py
+++ src/dbfuncs.py
@@ -7,6 +7,15 @@ def initialize_database():
     cur.execute("PRAGMA foreign_keys = ON;")
 
     cur.execute(
+        """CREATE TABLE IF NOT EXIST Ingredient (
+        ID INTEGER,
+        Name Text
+        PRIMARY KEY(ID, ASC)
+        ) 
+        """
+    )
+
+    cur.execute(
         """CREATE TABLE IF NOT EXIST Recipes (
         ID INTEGER, 
         Name TEXT UNIQUE NOT NULL,
@@ -17,14 +26,25 @@ def initialize_database():
         )
 
     cur.execute(
-        """CREATE TABLE IF NOT EXISIT Instructions (
+        """CREATE TABLE IF NOT EXIST Instruction (
         ID INTEGER,
         RecipeID INTEGER,
         Name TEXT NOT NULL,
         StepNumber INTEGER
-        PRIMARY KEY(!D, ASC)
+        PRIMARY KEY(ID, ASC)
+        FOREIGN KEY(RecipeID) REFERENCES Recipe(ID)
         )
         """
+    )
+
+    cur.execute(
+        """CREATE TABLE IF NOT EXIST Recipe_Ingredient_Link (
+        RecipeID INTEGER,
+        IngredientID INTEGER
+        FOREIGN KEY(RecipeID) REFERENCES Recipe(ID)
+        FOREIGN KEY(IngredientID) REFERENCES Ingredient(ID)
+        )
+         """
     )
 
 
