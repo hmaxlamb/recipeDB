8a615903540a5e61be09c9384f87ecd6a4221923
mlamb <h.max.lamb@gmail.com>
2025-03-12 22:10:21 -0400

    11. Added the instruction and ingredient class will make tests tomorrow

--- src/recipe.py
+++ src/recipe.py
@@ -4,17 +4,35 @@ import dbfuncs
 class Recipe:
    
    #init fuction to make recipe class
-    def __init__(name, category):
-        self.name = name
-        self.category = category
+    def __init__(self, name, category):
+        self.name = name.upper()
+        self.category = category.upper()
         self.ingredients = []
         self.intructions = []
 
     def __add_ingredient(ingredient):
         self.ingredients.append(ingredient)
 
-    def __add_intruction(instuction):
-        self.ingredients.append = []
+    def __append_instruction(instuction):
+        instuction.step_number = len(self.intructions) + 1
+        self.ingredients.append(intructions)
 
-    def _add to db():
+    #This fuction will insert the instrunction and -
+    #move the current instuction at the index  to the left
+    def __insert_instruction(instuction, step_num):
+        index = step_num - 1
+        self.instrunctions[index].step_number += 
+        instrunction.step_number = step_num
+
+        self.instrunctions.insert(index, instrunction)
+
         
+
+class Ingredient:
+    def __init__(self, name):
+        self.name = name.upper()
+
+class Instruction:
+    def __init__(self, desc, step_number):
+        self.desc = desc
+        self.step_number = step_number
