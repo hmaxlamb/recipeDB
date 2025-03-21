5f2f416d25436954e016be4375e6992a0013dbd6
mlamb <h.max.lamb@gmail.com>
2025-03-20 20:56:39 -0400

    37. made test for add recipe and fixed bugs

--- src/dbfuncs.py
+++ src/dbfuncs.py
@@ -9,35 +9,35 @@ def initialize_database():
 
     #Creates Ingredient Table
     cur.execute(
-        """CREATE TABLE IF NOT EXIST Ingredient (
+        """CREATE TABLE IF NOT EXISTS Ingredient (
         ID INTEGER,
         Name Text,
         Ammount FLOAT,
         Unit Text,
-        PRIMARY KEY(ID, ASC)
+        PRIMARY KEY(ID ASC)
         ) 
         """
     )
 
     #Creates Recipe Table
     cur.execute(
-        """CREATE TABLE IF NOT EXIST Recipe (
+        """CREATE TABLE IF NOT EXISTS Recipe (
         ID INTEGER, 
         Name TEXT UNIQUE NOT NULL,
         Category TEXT,
-        PRIMARY KEY(ID, ASC)
+        PRIMARY KEY(ID ASC)
         );
         """
         )
 
     #Creates Instruction Table
     cur.execute(
-        """CREATE TABLE IF NOT EXIST Instruction (
+        """CREATE TABLE IF NOT EXISTS Instruction (
         ID INTEGER,
         RecipeID INTEGER,
         Description TEXT NOT NULL,
-        StepNumber INTEGER
-        PRIMARY KEY(ID, ASC)
+        StepNumber INTEGER,
+        PRIMARY KEY(ID ASC),
         FOREIGN KEY(RecipeID) REFERENCES Recipe(ID)
         )
         """
@@ -45,10 +45,10 @@ def initialize_database():
 
     #Creates a table that links recipes to ingredients
     cur.execute(
-        """CREATE TABLE IF NOT EXIST Recipe_Ingredient_Link (
+        """CREATE TABLE IF NOT EXISTS Recipe_Ingredient_Link (
         RecipeID INTEGER,
-        IngredientID INTEGER
-        FOREIGN KEY(RecipeID) REFERENCES Recipe(ID)
+        IngredientID INTEGER,
+        FOREIGN KEY(RecipeID) REFERENCES Recipe(ID),
         FOREIGN KEY(IngredientID) REFERENCES Ingredient(ID)
         )
          """
@@ -58,14 +58,13 @@ def initialize_database():
     conn.close()
 
 #Fuction adds a list of ingredients and returns the ids in a list
-def add_ingredients(ing_list):
+def add_ingredients(conn, ing_list):
     data_ar = []
     for ing in ing_list:
         data_ar.append({"Name": ing.name, "Ammount": ing.ammount, "Unit": ing.unit})
 
     data_tp = tuple(data_ar)
 
-    conn = sqlite3.connect("recipe_database.db")
     cur = conn.cursor()
 
     cur.executemany("INSERT INTO Ingredient (Name, Amount, Unit) VALUES (:Name, :Ammount, :Unit) RETURNING ID", data_tp)
@@ -77,27 +76,21 @@ def add_ingredients(ing_list):
     return id_list
 
 #adds a recipe to table
-def add_recipe(recp):
+def add_recipe(conn, recp):
     data = {"Name": recp.name, "Category": recp.category}
-
-    conn = sqlite3.connect("recipe_database.db")
     cur = conn.cursor()
 
     cur.execute("INSERT INTO Recipe (Name, Category) VALUES (:Name, :Category)", data)
     recipe_id = cur.lastrowid
 
-    conn.commit()
-    conn.close()
-
-    return recipe_id
+    return int(recipe_id)
 
 #adds the link between ingredients and a recipe
-def add_ingredient_recipe_link(resp_id, ingred_id_list):
+def add_ingredient_recipe_link(conn, resp_id, ingred_id_list):
     data = []
     for igred_id in ingred_id_list:
         data.append({"RecipeID": resp_id, "IngredientID": igred_id})
 
-    conn = sqlite3.connect("recipe_database.db")
     cur = conn.cursor()
 
     cur.executemany("INSERT INTO Recipe_Ingredient_Link (RecipeID, IngredientID) VALUES (:RecipeID, :IngredientID)", data)
@@ -105,12 +98,11 @@ def add_ingredient_recipe_link(resp_id, ingred_id_list):
     conn.commit()
     conn.close()
 
-def add_instructions_list(recp_ID, instruct_list):
+def add_instructions_list(conn, recp_ID, instruct_list):
     data = []
     for instruct in instruct_list:
         data.append({"RecipeID": recp_ID, "Description": instruct.desc})
 
-    conn = sqlite3.connect("recipe_database.db")
     cur = conn.cursor()
 
     cur.executemany("INSERT INTO Instruction (RecipeID, Description) VALUES (:RecipeID, :Description)", data)
--- src/test_dbfuncs.py
+++ src/test_dbfuncs.py
@@ -1,44 +1,46 @@
 import unittest
-import dbfuncs
+import sqlite3
+from dbfuncs import add_recipe
+from recipe import Recipe
 
 class TestDBFunctions(unittest.TestCase):
     def setUp(self):
         
         self.conn = sqlite3.connect(":memory:")
-        self.cur = conn.cursor()
+        self.cur = self.conn.cursor()
         self.cur.execute("PRAGMA foreign_keys = ON;")
 
         #Creates Ingredient Table
         self.cur.execute(
-            """CREATE TABLE IF NOT EXIST Ingredient (
+            """CREATE TABLE IF NOT EXISTS Ingredient (
             ID INTEGER,
             Name Text,
             Ammount FLOAT,
             Unit Text,
-            PRIMARY KEY(ID, ASC)
+            PRIMARY KEY(ID)
             ) 
             """
         )
 
         #Creates Recipe Table
         self.cur.execute(
-            """CREATE TABLE IF NOT EXIST Recipe (
+            """CREATE TABLE IF NOT EXISTS Recipe (
             ID INTEGER, 
             Name TEXT UNIQUE NOT NULL,
             Category TEXT,
-            PRIMARY KEY(ID, ASC)
+            PRIMARY KEY(ID)
             );
             """
             )
 
         #Creates Instruction Table
         self.cur.execute(
-            """CREATE TABLE IF NOT EXIST Instruction (
+            """CREATE TABLE IF NOT EXISTS Instruction (
             ID INTEGER,
             RecipeID INTEGER,
             Description TEXT NOT NULL,
-            StepNumber INTEGER
-            PRIMARY KEY(ID, ASC)
+            StepNumber INTEGER,
+            PRIMARY KEY(ID),
             FOREIGN KEY(RecipeID) REFERENCES Recipe(ID)
             )
             """
@@ -46,20 +48,30 @@ class TestDBFunctions(unittest.TestCase):
 
         #Creates a table that links recipes to ingredients
         self.cur.execute(
-            """CREATE TABLE IF NOT EXIST Recipe_Ingredient_Link (
+            """CREATE TABLE IF NOT EXISTS Recipe_Ingredient_Link (
             RecipeID INTEGER,
-            IngredientID INTEGER
-            FOREIGN KEY(RecipeID) REFERENCES Recipe(ID)
+            IngredientID INTEGER,
+            FOREIGN KEY(RecipeID) REFERENCES Recipe(ID),
             FOREIGN KEY(IngredientID) REFERENCES Ingredient(ID)
             )
             """
-        )
+        ) 
+
+        self.conn.commit()
     
     def tearDown(self):
         self.conn.close()
-
+    
     def test_add_recipe(self):
-        pass
+        recp = Recipe("PIZZA", "ITALAIN")
+        id = add_recipe(self.conn, recp)
+
+        self.cur.execute("SELECT * FROM Recipe WHERE ID = ?", (id,))
+        result = self.cur.fetchone()
+
+        self.assertEqual(result[1], "PIZZA")
+    
+        
 
 if __name__ == '__main__':
     unittest.main()
